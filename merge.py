82d8f492a1d0311637de68a7727d6f5d3db9651b
Sabrina <sssedovic@uchicago.edu>
2021-05-14 14:56:04 -0500

    actual merge draft

--- merge.py
+++ merge.py
@@ -3,4 +3,14 @@
 
 import pandas as pd
 
-pd.merge()
+EMP_IEP = pd.read_csv("emp_iep.csv")
+IDTYPES = {"six_sch_id": "Dept/Unit Number", "four_sch_id": "Unit_2",
+           "building_id": "School ID"}
+
+def merging(df1, df2, df2_colname, df2idtype="six_sch_id",
+            df3, df3_colname, df3idtype="six_sch_id"):
+    sub_pd = df1.merge(df2, how='left', left_on=IDTYPES[df2idtype],
+                       right_on=df2_colname)
+    full_pd = sub_pd.merge(df3, how='left', left_on=IDTYPES[df3idtype],
+                        right_on=df3_colname)
+    full_pd.to_csv("school_data.csv")
