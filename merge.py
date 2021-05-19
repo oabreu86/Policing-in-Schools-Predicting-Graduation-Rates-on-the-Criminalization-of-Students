6c8f32984c4477dffd6f152df754b021fc4f3956
Sabrina <sssedovic@uchicago.edu>
2021-05-19 08:49:46 -0500

    merged

--- merge.py
+++ merge.py
@@ -4,8 +4,12 @@
 import pandas as pd
 
 EMP_IEP = pd.read_csv("data/emp_iep.csv")
-IDTYPES = {"six_sch_id": "Dept/Unit Number", "four_sch_id": "Unit_2",
-           "building_id": "School ID"}
+SUS_EXP = pd.read_csv("data/SuspensionsExpulsionsPoliceContacts.csv")
+SUS_COLS = ["School ID", "School Year"]
+ATT_GR = pd.read_csv("data/attendance_gradrates.csv")
+ATT_COLS = ["School ID", "Year"]
+IDTYPES = {"six_sch_id": "School ID", "four_sch_id": "Unit_2",
+           "building_id": "Dept/Unit Number"}
 
 def merging(df1, df2, df2_colname,df3, df3_colname, 
             df2idtype="six_sch_id", df3idtype="six_sch_id"):
@@ -14,3 +18,9 @@ def merging(df1, df2, df2_colname,df3, df3_colname,
     full_pd = sub_pd.merge(df3, how='left', left_on=[IDTYPES[df3idtype], "Year"],
                         right_on=df3_colname)
     full_pd.to_csv("school_data.csv")
+    print(sub_pd.head())
+    return full_pd
+
+
+def run():
+    return merging(EMP_IEP, SUS_EXP, SUS_COLS, ATT_GR, ATT_COLS)
