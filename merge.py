062a66a6796366ac452a824771ffe37f4ddd5926
Sabrina <sssedovic@uchicago.edu>
2021-05-19 21:34:15 -0500

    second merge attempt

--- merge.py
+++ merge.py
@@ -6,7 +6,7 @@ import pandas as pd
 EMP_IEP = pd.read_csv("data/emp_iep.csv")
 SUS_EXP = pd.read_csv("data/SuspensionsExpulsionsPoliceContacts.csv")
 SUS_COLS = ["School ID", "School Year"]
-ATT_GR = pd.read_csv("data/attendance_gradrates.csv")
+ATT_GR = pd.read_csv("data/attendance_gradrates2.csv")
 ATT_COLS = ["School ID", "Year"]
 IDTYPES = {"six_sch_id": "School ID", "four_sch_id": "Unit_2",
            "building_id": "Dept/Unit Number"}
@@ -17,8 +17,8 @@ def merging(df1, df2, df2_colname,df3, df3_colname,
                        right_on=df2_colname)
     full_pd = sub_pd.merge(df3, how='left', left_on=[IDTYPES[df3idtype], "Year"],
                         right_on=df3_colname)
+    full_pd = full_pd[full_pd["Year"] != 2010]
     full_pd.to_csv("school_data.csv")
-    print(sub_pd.head())
     return full_pd
 
 
