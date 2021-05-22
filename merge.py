2551b125ce9dffb5289f918858b358c3e865ddf5
Sabrina <sssedovic@uchicago.edu>
2021-05-22 14:15:15 -0500

    changed year

--- merge.py
+++ merge.py
@@ -4,6 +4,8 @@
 import pandas as pd
 
 EMP_IEP = pd.read_csv("data/emp_iep.csv")
+RACE_DEMO = pd.read_csv("data/RacialDemographics.csv")
+RACE_COL = ["School ID", "Year"]
 SUS_EXP = pd.read_csv("data/SuspensionsExpulsionsPoliceContacts.csv")
 SUS_COLS = ["School ID", "School Year"]
 ATT_GR = pd.read_csv("data/attendance_gradrates2.csv")
@@ -11,11 +13,14 @@ ATT_COLS = ["School ID", "Year"]
 IDTYPES = {"six_sch_id": "School ID", "four_sch_id": "Unit_2",
            "building_id": "Dept/Unit Number"}
 
-def merging(df1, df2, df2_colname,df3, df3_colname, 
-            df2idtype="six_sch_id", df3idtype="six_sch_id"):
-    sub_pd = df1.merge(df2, how='left', left_on=[IDTYPES[df2idtype], "Year"],
+def merging(df1, df2, df2_colname,df3, df3_colname, df4, df4_colname, 
+            idtype="six_sch_id"):
+    df3["Year"] = df3["Year"] + 1
+    sub_pd = df1.merge(df2, how='left', left_on=[IDTYPES[idtype], "Year"],
                        right_on=df2_colname)
-    full_pd = sub_pd.merge(df3, how='left', left_on=[IDTYPES[df3idtype], "Year"],
+    sub_pd = sub_pd.merge(df4, how='left', left_on=[IDTYPES[idtype], "Year"],
+                        right_on=df4_colname)
+    full_pd = sub_pd.merge(df3, how='left', left_on=[IDTYPES[idtype], "Year"],
                         right_on=df3_colname)
     full_pd = full_pd[full_pd["Year"] != 2010]
     full_pd.to_csv("school_data.csv")
@@ -23,4 +28,5 @@ def merging(df1, df2, df2_colname,df3, df3_colname,
 
 
 def run():
-    return merging(EMP_IEP, ATT_GR, ATT_COLS,  SUS_EXP, SUS_COLS)
+    return merging(EMP_IEP, ATT_GR, ATT_COLS,  SUS_EXP, SUS_COLS,
+                   RACE_DEMO, RACE_COL)
